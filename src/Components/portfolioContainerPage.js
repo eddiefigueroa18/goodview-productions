f8debd565067a5330c0ed1e4d5bf6a91ef9a4314
eddiefigueroa18 <eddiefigueroa18@gmail.com>
2023-03-09 10:48:20 -0800

    added syntax to display main page with header and footer. Also, set current page to be the homepage

--- src/Components/portfolioContainerPage.js
+++ src/Components/portfolioContainerPage.js
@@ -2,3 +2,41 @@ import react, { useState } from "react";
 
 import Header from "./Header";
 import Footer from "./Footer";
+
+import Homepage from "./Pages/Homepage";
+import Gallery from "./Pages/Gallery";
+import Contact from "./Pages/Contact";
+
+export default function portfolioContainerPage() {
+    const [currentPage, setCurrentPage] = useState (Homepage);
+
+
+    const renderPage = () => {
+        if (currentPage === Homepage) {
+            return <Homepage />;
+        }
+        if (currentPage === Gallery) {
+            return <Gallery />;
+        }
+        if (currentPage === Contact) {
+            return <Contact />;
+        }
+    };
+
+
+    const handlePageChange = (page) => setCurrentPage(page);
+
+    return (
+        <div className="mainBody">
+
+            <Header currentPage={currentPage} handlePageChange={handlePageChange} />
+
+                {renderPage()}
+
+            <Footer currentPage={currentPage} handlePageChange={handlePageChange} />
+            
+        </div>
+    );
+
+
+}
